bdb151f23201aa48babb622e9efc94bba63cd1e6
kylerlittle <kyler.little@wsu.edu>
2018-05-22 20:35:00 -0700

    navbar and routing complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './css/App.css';
+import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
---
+++ src/pages/Signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signin extends Component {
+    render() {
+	return (
+		<div className="Signin">
+		<p>This is my sign-in page.</p>
+		</div>
+	);
+    }
+}
+
+export default Signin;
