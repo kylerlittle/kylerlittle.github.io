f6142793ef5fac4adb80066a4a90487e253abdd8
kylerlittle <kyler.little@wsu.edu>
2018-08-24 18:35:11 -0700

    WIP-- responsive navbar

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -3,6 +3,10 @@ import { Link, withRouter } from 'react-router-dom';
 import './Navbar.css';
 
 class Navbar extends Component {
+	constructor(props) {
+		super(props);
+		this.toggleResponsive = this.toggleResponsive.bind(this);
+	}
     componentWillMount() {
 	// Read more here: https://medium.com/@AkyunaAkish/understanding-react-router-4-df73a66d96c4
 	this.props.history.listen(() => {
@@ -11,16 +15,25 @@ class Navbar extends Component {
     }
     componentWillUnmount() {
 	this.props.history.unlisten();
-    }
+	}
+	toggleResponsive() {
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+			x.className += " responsive";
+		} else {
+			x.className = "topnav";
+		}
+	}
     render() {
 	return (
 		<nav>
-		<ul className="topnav">
-		<li style={{float: "left"}}><Link to="/"> Home </Link></li>
-		<li style={{float: "left"}}><Link to="/thoughts"> Thoughts </Link></li>
-		<li style={{float: "left"}}><Link to="/travels"> Travels </Link></li>
-		<li style={{float: "left"}}><Link to="/about"> About </Link></li>
-		<li style={{float: "right"}} className="active"><Link to="/signin"> Sign In </Link></li>
+		<ul className="topnav" id="myTopnav">
+		<li><Link to="/"> Home </Link></li>
+		<li><Link to="/projects"> Projects </Link></li>
+		<li><Link to="/languages"> Languages </Link></li>
+		<li><Link to="/experience"> Experience </Link></li>
+		<li className="active"><Link to="/thoughts"> Thoughts </Link></li>
+		<li className="icon" onClick={this.toggleResponsive}><i className="fa fa-bars"></i></li>
 		</ul>
 		</nav>
 	);
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import Thoughts from '../pages/Thoughts';
-import Travels from '../pages/Travels';
-import About from '../pages/About';
-import Signin from '../pages/Signin';
+import Projects from '../pages/Projects';
+import Experience from '../pages/Experience';
+import Languages from '../pages/Languages';
 
 class Main extends Component {
     render() {
@@ -13,9 +13,9 @@ class Main extends Component {
 	<Switch>
 	<Route exact path="/" component={() => (<Home title="Home" />)} />
 	<Route path="/thoughts" component={Thoughts} />
-	<Route path="/travels" component={Travels} />
-	<Route path="/about" component={About} />
-	<Route path="/signin" component={Signin} />
+	<Route path="/projects" component={Projects} />
+	<Route path="/experience" component={Experience} />
+	<Route path="/languages" component={Languages} />
 		</Switch>
 	</main>
 	);
