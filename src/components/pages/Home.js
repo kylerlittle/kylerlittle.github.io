2a820458af5f05d9f3149b91fea7d3195c7466c1
kylerlittle <kyler.little@wsu.edu>
2018-09-01 15:13:37 -0700

    updated mobile navbar; mobile and comp should display img correctly now too

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
 		<div className="Home">
 		<h1>{this.props.displayName}</h1>
 		<p>My name is <b>Kyler Little</b>. Welcome to my personal website.</p>
-		<img className="pic-of-me" src={require('./assets/myself.JPG')} alt="This is a picture of me holding a sloth in the Amazon Jungle."/>
+		<img className="pic-of-me" src={require('./assets/myself.JPG')} alt="I am holding a sloth in the Amazon Jungle."/>
 		<p>Testing text post-image. This should show up.</p>
 		<p>This is some text to make the page scrollable.</p>
 		<p>This is some text to make the page scrollable.</p>
