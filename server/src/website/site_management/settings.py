6d1662d08ab9954aa69c3d9d5051ffcf7826fd78
kylerlittle <kyler.little@wsu.edu>
2019-06-18 21:32:16 -0700

    basic blog db set up

--- server/src/website/site_management/settings.py
+++ server/src/website/site_management/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'site_content.apps.SiteContentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
