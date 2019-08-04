c3bebc5516ced84293b02e8b996900283d17f509
kylerlittle <kyler.little@wsu.edu>
2019-08-03 23:54:37 -0700

    update aboutme to be cooler

---
+++ website/site_content/static/js/aboutme.js
@@ -0,0 +1,73 @@
+/**
+ * On hover of img
+ */
+function onMouseEnter() {
+    var img = this.children[0];
+    img.style.filter = "brightness(30%)";
+
+    var caption = img.nextElementSibling;
+    caption.style.opacity = "1";
+}
+
+function onMouseLeave() {
+    var img = this.children[0];
+    img.style.filter = "brightness(100%)";
+
+    var caption = img.nextElementSibling;
+    caption.style.opacity = "0";
+}
+
+$(".img-container").hover(onMouseEnter, onMouseLeave);
+
+// https://codepen.io/gschier/pen/jkivt
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+  };
